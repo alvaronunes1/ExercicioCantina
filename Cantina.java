448ce619ffb0165e10e19cd4c24b6d750342d82f
12116455 <alvaro.nunes15@gmail.com>
2023-04-11 21:33:28 -0300

    feat: Try and Catch adicionado

--- Cantina.java
+++ Cantina.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class Cantina {
     public static void main(String[] args) {
 
+
         String nomeCliente = JOptionPane.showInputDialog("Nome do cliente: ");
         String tipoMassa = JOptionPane.showInputDialog("Nome da massa: ");
         Double valorMassa = Double.parseDouble(JOptionPane.showInputDialog("Valor da massa:  "));
@@ -20,33 +21,41 @@ public class Cantina {
 
 
         Ingrediente ingredientes = new Ingrediente();
+        try {
 
-        while(!menu.equals("5")) {
-
-            menu = JOptionPane.showInputDialog("\n 1 - Bacon\n 2 - Mussarela\n 3 - Tomate\n 4 - Queijo\n 5 - Sair ");
+            while (!menu.equals("5")) {
 
 
-            if (menu.equals("1")) {
-                ingredientes.nome = "Bacon";
+                if (menu.equals("1")) {
+                    ingredientes.nome = "Bacon";
+                }
+                if (menu.equals("2")) {
+                    ingredientes.nome = "Mussarela";
+                }
+
+                if (menu.equals("3")) {
+                    ingredientes.nome = "Tomate";
+                }
+
+                if (menu.equals("4")) {
+                    ingredientes.nome = "Queijo";
+                }
+
+
+                if (!(menu.equals("1") && menu.equals("2") && menu.equals("3") && menu.equals("4"))) {
+                    throw new Exception("Não existe o ingrediente selecionado");
+                }
+
+                menu = JOptionPane.showInputDialog("\n 1 - Bacon\n 2 - Mussarela\n 3 - Tomate\n 4 - Queijo\n 5 - Sair ");
+
+
+                Double valorFinal = massa.valor + 2;
+
+                JOptionPane.showMessageDialog(null, "Pedido feito para " + cliente.nome + "\n" + massa.tipo + " com " + ingredientes.nome + "\n Valor: " + valorFinal);
             }
-            if (menu.equals("2")) {
-                ingredientes.nome = "Mussarela";
-            }
-
-            if (menu.equals("3")) {
-                ingredientes.nome = "Tomate";
-            }
-
-            if (menu.equals("4")) {
-                ingredientes.nome = "Queijo";
-            }
-
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
-
-        Double valorFinal = massa.valor + 2;
-
-        JOptionPane.showMessageDialog(null, "Pedido feito para " + cliente.nome + "\n" + massa.tipo + " com " + ingredientes.nome + "\n Valor: " + valorFinal);
-
 
     }
 }
